6f4aa531c8408b2a6680629f5d0fb313899a3323
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2025-02-19 11:26:40 +0300

    feat(config): core listchars

--- .config/nvim/lua/core/settings.lua
+++ .config/nvim/lua/core/settings.lua
@@ -2,19 +2,8 @@ local cmd = vim.cmd -- execute Vim commands
 local opt = vim.opt
 local g = vim.g
 
--- make nvim open directory is selected nvim dir
--- local group_cdpwd = vim.api.nvim_create_augroup("group_cdpwd", { clear = true })
--- vim.api.nvim_create_autocmd("VimEnter", {
--- 	group = group_cdpwd,
--- 	pattern = "*",
--- 	callback = function()
--- 		vim.api.nvim_set_current_dir(vim.fn.expand("%:p:h"))
--- 	end,
--- })
-
 opt.termguicolors = true
 g.netrw_banner = 0
--- vim.cmd([[colorscheme effectivnayarabota1__light]]) -- eta ident
 
 opt.colorcolumn = "80" -- Разделитель на 80 символов
 opt.cursorline = true -- Подсветка строки с курсором
@@ -30,14 +19,12 @@ opt.ignorecase = true -- Игнорировать регистр при поис
 opt.smartcase = true -- Не игнорировать регистр, если есть символы в верхнем регистре
 opt.showmatch = true -- Подсвечивать найденные текстовые объекты
 
-opt.shiftwidth = 2 -- shift 4 spaces when tab
-opt.tabstop = 2 -- 1 tab == 4 spaces
+opt.shiftwidth = 4 -- shift 4 spaces when tab
+opt.tabstop = 4 -- 1 tab == 4 spaces
 opt.smartindent = true -- Подстраивать новые строки под предыдущий отступ
--- opt.expandtab = false -- nouse spaces instead of tabs
 --
--- Tab separator setup
-vim.opt.list = true
-vim.opt.listchars = { eol = "󰘌", tab = "  " }
+opt.list = true
+opt.listchars = { eol = "󰘌", tab = "--.", trail="T", leadmultispace=". ", extends="#", precedes="#" }
 
 vim.cmd([[au BufRead,BufNewFile *.eta set filetype=eta]]) -- eta ident
 
