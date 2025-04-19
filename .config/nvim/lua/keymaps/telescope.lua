52c28c4fc2f28ff19dea66f33b219989bc573974
Kirill Ivanov <iwwwanow.work@yandex.ru>
2025-04-20 02:31:39 +0300

    feat(common): update config

--- .config/nvim/lua/keymaps/telescope.lua
+++ .config/nvim/lua/keymaps/telescope.lua
@@ -7,5 +7,11 @@ vim.keymap.set(
 	{}
 )
 vim.keymap.set("n", "<Space>fg", builtin_telescope.live_grep, {})
+vim.keymap.set("n", "<Space>fG", function()
+	builtin_telescope.live_grep({
+		additional_args = { "--hidden" },
+		prompt_title = "🔍 Grep in Hidden Files"
+	})
+end, { desc = "Live grep in hidden files" })
 vim.keymap.set("n", "<Space>fb", builtin_telescope.buffers, {})
 -- vim.keymap.set("n", "<Space>fh", builtin_telescope.help_tags, {})
