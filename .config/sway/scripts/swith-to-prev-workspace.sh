a35f844ae4f4deb6801acd9afa9aae6f4361992a
Kirill Ivanov <iwwwanow.work@yandex.ru>
2025-10-22 20:37:08 +0300

    feat(config): sway scripts

--- .config/sway/scripts/swith-to-prev-workspace.sh
+++ .config/sway/scripts/swith-to-prev-workspace.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 current_workspace=$(swaymsg -t get_workspaces | jq '.[] | select(.focused == true) | .num')
-new_workspace=$((current_workspace + 1))
+new_workspace=$((current_workspace - 1))
 swaymsg workspace $new_workspace
