f8eb0a9da4836ac74a07c7f06f28105c58c0ce97
Kirill Ivanov <iwwwanow.work@yandex.ru>
2025-11-05 17:49:57 +0300

    changes from ubuntu-host on Wed Nov 5 05:49:57 PM MSK 2025

--- scripts/setup-all-sync.sh
+++ scripts/setup-all-sync.sh
@@ -6,6 +6,7 @@ echo "Setting up all git-sync services..."
 $(dirname "$0")/setup-dotfiles-sync.sh
 echo "---"
 $(dirname "$0")/setup-passwords-sync.sh
+echo "---"
 
 echo "All sync services installed!"
 echo "Check status: systemctl status dotfiles-sync pass-sync"
